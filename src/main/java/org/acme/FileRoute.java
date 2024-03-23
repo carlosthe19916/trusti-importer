379ade9cdfde81a2ddb0f6ca1e537006dc6fb780
carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>
2024-03-23 12:42:57 +0100

    Download file camel

---
+++ src/main/java/org/acme/FileRoute.java
@@ -0,0 +1,92 @@
+package org.acme;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.Route;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.http.base.HttpOperationFailedException;
+import org.apache.camel.support.RoutePolicySupport;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@ApplicationScoped
+public class FileRoute extends RouteBuilder {
+
+    private static final Pattern HREF_PATTERN = Pattern.compile("href=\"(.*?)\"");
+
+    @Override
+    public void configure() throws Exception {
+        from("direct:discover-url-directory")
+                .onException(IOException.class)
+                    .useOriginalMessage()
+                    .maximumRedeliveries(2)
+                    .redeliveryDelay(200)
+                    .logExhausted(false)
+                    .log(LoggingLevel.ERROR, "Could not reach the given URL  ${body}")
+                .end()
+
+                .setHeader("base_url", body())
+                .process(exchange -> {
+                    String url = exchange.getIn().getBody(String.class);
+
+                    Scanner out = new Scanner(new URL(url).openStream(), StandardCharsets.UTF_8).useDelimiter("\n");
+                    List<String> hrefs = new ArrayList<>(8);
+
+                    while (out.hasNext()) {
+                        Matcher match = HREF_PATTERN.matcher(out.next());
+                        if (match.find()) {
+                            hrefs.add(match.group(1));
+                        }
+                    }
+
+                    out.close();
+
+                    exchange.getIn().setBody(hrefs);
+                })
+                .split(body()).parallelProcessing()
+                    .filter(body().not().contains("../"))
+                    .process(exchange -> {
+                        String url = exchange.getIn().getHeader("base_url", String.class);
+                        String next = exchange.getIn().getBody(String.class);
+                        exchange.getIn().setBody(url + next);
+                    })
+                    .choice()
+                        .when(body().endsWith("/"))
+                            .to("direct:discover-url-directory")
+                        .endChoice()
+                        .when(body().endsWith(".json"))
+                            .to("direct:download-url")
+                        .endChoice()
+                        .otherwise()
+                            .log(LoggingLevel.TRACE, "Ignoring ${body}")
+                        .endChoice()
+                    .end()
+                .end();
+
+        from("direct:download-url")
+                .setHeader("file_url", body())
+                .log(LoggingLevel.INFO, "Downloading ${body}")
+                .setHeader(Exchange.HTTP_METHOD, constant("GET"))
+                .setHeader(Exchange.CONTENT_TYPE, constant("text/plain"))
+                .toD("${body}")
+                .setBody(header(Exchange.HTTP_RESPONSE_CODE))
+                .onException(HttpOperationFailedException.class)
+                    .maximumRedeliveries(2)
+                    .redeliveryDelay(0)
+                    .handled(true)
+                    .logExhausted(false)
+                    .logExhaustedMessageBody(false)
+                    .log(LoggingLevel.ERROR, "${header.file_url}")
+                .end();
+    }
+
+}
